749e4dc27dbb0527aace494f29e2b2456abbc86f
osvaldosereia <Junior.sereia@hotmail.com>
2025-09-16 14:28:48 -0300

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+// sw.js - Service Worker básico para cache offline
+const CACHE_NAME = "direito-love-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/favicon-32.png",
+  "./icons/favicon-192.png",
+  "./icons/apple-touch-icon.png"
+];
+
+// Instalação
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+// Ativação
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
