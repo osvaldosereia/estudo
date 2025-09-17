0539562142c5130327d7d5f0920b6cbd2186a7be
osvaldosereia <Junior.sereia@hotmail.com>
2025-09-17 14:42:28 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker revisado
-const CACHE_NAME = "direito-love-v10"; // 🔄 altere a versão SEMPRE que fizer update
+const CACHE_NAME = "direito-love-v11"; // 🔄 altere a versão SEMPRE que fizer update
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
