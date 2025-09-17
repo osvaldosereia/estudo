3cca5f2d6b99ab41180aca223d3d69854fa19312
osvaldosereia <Junior.sereia@hotmail.com>
2025-09-17 08:40:46 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker básico para cache offline
-const CACHE_NAME = "direito-love-v2";
+const CACHE_NAME = "direito-love-v3";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
